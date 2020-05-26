60895365a016d53a5ac1c1cfaec2ab866a30bd4d
ilra <ilari.raisanen@tuni.fi>
2020-05-26 13:51:39 +0300

    Tehtävä 2.8 tehty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,22 +3,25 @@ import React, { useState } from 'react'
 const Person = (props) =>{
   return(
     <div>
-        {props.name}
+        {props.name} {props.number}
     </div>
   )
 }
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', 
+      number: '040-1234567'}
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) =>{
     event.preventDefault()
 
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     const exists = 
     persons.filter(person=>person.name===newName)
@@ -29,10 +32,15 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
     
     setNewName('')
+    setNewNumber('')
   }
 
-  const handlePersonChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -42,10 +50,17 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName}
-          onChange={handlePersonChange}/>
+          onChange={handleNameChange}
+          />
+        </div>
+        <div>number: <input value={newNumber}
+         onChange={handleNumberChange}
+         />
         </div>
         <div>
-          <button type="submit">add</button>
+          
+          <button type="submit">add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
@@ -53,7 +68,8 @@ const App = () => {
       {persons.map(person =>
           <Person 
           key={person.name}
-          name={person.name} />
+          name={person.name}
+          number={person.number} />
         )}
      </div>
     </div>
