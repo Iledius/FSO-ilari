b9fe1f7ccc938e7e854dfd9e0809f84e479e6f6e
ilra <ilari.raisanen@tuni.fi>
2020-05-26 11:47:25 +0300

    Tehtävä 2.6 tehty

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+
+const Person = (props) =>{
+  return(
+    <div>
+        {props.name}
+    </div>
+  )
+}
+
+
+
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addPerson = (event) =>{
+    event.preventDefault()
+
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <div>debug: {newName}</div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName}
+          onChange={handlePersonChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+      {persons.map(person =>
+          <Person 
+          key={person.name}
+          name={person.name} />
+        )}
+     </div>
+    </div>
+  )
+}
+
+export default App
