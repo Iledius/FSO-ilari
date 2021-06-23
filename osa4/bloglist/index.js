98fa53eb38d08614c5387551168ac786e74450c0
Iledius <raisanen.ilari@outlook.com>
2021-06-23 18:09:21 +0300

    started week 4

---
+++ osa4/bloglist/index.js
@@ -0,0 +1,9 @@
+const app = require("./app")
+const http = require("http")
+const config = require("./utils/config")
+const logger = require("./utils/logger")
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
