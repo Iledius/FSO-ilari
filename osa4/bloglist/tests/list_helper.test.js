92ea1e4c80a87d5399118a055bf32d6f0b9398da
Iledius <raisanen.ilari@outlook.com>
2021-06-25 14:55:41 +0300

    4.6: author with most posts function added.

--- osa4/bloglist/tests/list_helper.test.js
+++ osa4/bloglist/tests/list_helper.test.js
@@ -1,5 +1,74 @@
 const listHelper = require("../utils/list_helper")
 
+const blogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0,
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0,
+  },
+  {
+    _id: "5a422b891b54a676234d17fa",
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+    __v: 0,
+  },
+  {
+    _id: "5a422ba71b54a676234d17fb",
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+    __v: 0,
+  },
+  {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+    __v: 0,
+  },
+]
+const oneBlog = [
+  {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+    __v: 0,
+  },
+]
+const mostLikedPost = {
+  _id: "5a422b3a1b54a676234d17f9",
+  title: "Canonical string reduction",
+  author: "Edsger W. Dijkstra",
+  url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+  likes: 12,
+  __v: 0,
+}
+
 test("dummy returns one", () => {
   const blogs = []
 
@@ -8,66 +77,6 @@ test("dummy returns one", () => {
 })
 
 describe("total likes", () => {
-  const blogs = [
-    {
-      _id: "5a422a851b54a676234d17f7",
-      title: "React patterns",
-      author: "Michael Chan",
-      url: "https://reactpatterns.com/",
-      likes: 7,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0,
-    },
-    {
-      _id: "5a422b3a1b54a676234d17f9",
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-      __v: 0,
-    },
-    {
-      _id: "5a422b891b54a676234d17fa",
-      title: "First class tests",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-      likes: 10,
-      __v: 0,
-    },
-    {
-      _id: "5a422ba71b54a676234d17fb",
-      title: "TDD harms architecture",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 0,
-      __v: 0,
-    },
-    {
-      _id: "5a422bc61b54a676234d17fc",
-      title: "Type wars",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-      likes: 2,
-      __v: 0,
-    },
-  ]
-  const oneBlog = [
-    {
-      _id: "5a422bc61b54a676234d17fc",
-      title: "Type wars",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-      likes: 2,
-      __v: 0,
-    },
-  ]
   test("when list has many blogs, equals the likes of those", () => {
     const result = listHelper.totalLikes(blogs)
     expect(result).toBe(36)
@@ -75,5 +84,20 @@ describe("total likes", () => {
   test("when list has one blog, equals the likes of that", () => {
     const result = listHelper.totalLikes(oneBlog)
     expect(result).toBe(2)
+  })
+})
+
+describe("most liked", () => {
+  test("likes most liked post has, all testblogs", () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual(mostLikedPost)
+  })
+})
+
+describe("most blog posts", () => {
+  test("author with most posts and amount of posts, testlist used", () => {
+    const result = listHelper.mostBlogs(blogs)
+    console.log(result)
+    expect(result).toEqual({ "Robert C. Martin": 3 })
   })
 })
--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -8,7 +8,32 @@ const totalLikes = (blogs) => {
   return sum
 }
 
+const favoriteBlog = (blogs) => {
+  const mostLiked = blogs.reduce((prev, current) => {
+    return prev.likes > current.likes ? prev : current
+  })
+  return mostLiked
+}
+
+const mostBlogs = (blogs) => {
+  let postsPerPerson = []
+  blogs.map((blog) => {
+    postsPerPerson[blog.author] = postsPerPerson[blog.author]
+      ? postsPerPerson[blog.author] + 1
+      : 1
+  })
+  console.log(postsPerPerson)
+  let maxAuthor
+  Object.keys(postsPerPerson).reduce(
+    (prev, current) =>
+      (maxAuthor =
+        postsPerPerson[prev] > postsPerPerson[current] ? prev : current)
+  )
+  return { [maxAuthor]: postsPerPerson[maxAuthor] }
+}
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
+  mostBlogs,
 }
