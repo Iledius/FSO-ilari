b0f20856b350be606670c4efe8a5f372934eaf9f
Iledius <raisanen.ilari@outlook.com>
2021-06-29 14:56:01 +0300

    4.14 done: updating and deleting added to api

--- osa4/bloglist/controllers/blogs.js
+++ osa4/bloglist/controllers/blogs.js
@@ -1,25 +1,22 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({})
-    .then((blogs) => {
-      response.json(blogs)
-    })
-    .catch((error) => console.error(error))
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs.map((blog) => blog.toJSON()))
 })
 
 blogsRouter.post("/", async (request, response) => {
-  const b = request.body._doc
-  if (!b.title && !b.url) {
+  const body = request.body._doc
+  if (!body.title && !body.url) {
     response.send(400).end()
   }
 
   const blog = new Blog({
-    title: b.title,
-    author: b.author,
-    url: b.url,
-    likes: b.likes || 0,
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
   })
 
   const savedBlog = await blog.save()
@@ -33,6 +30,25 @@ blogsRouter.get("/:id", async (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put("/:id", async (request, response) => {
+  const body = request.body._doc
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  })
+
+  await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
