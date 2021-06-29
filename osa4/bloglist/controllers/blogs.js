0cb990dbe58bd32bb244e4a82496f81b357a70ce
Iledius <raisanen.ilari@outlook.com>
2021-06-29 21:30:36 +0300

    4.19: added auth with jwt

--- osa4/bloglist/controllers/blogs.js
+++ osa4/bloglist/controllers/blogs.js
@@ -1,13 +1,33 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
+const User = require("../models/user")
+const jwt = require("jsonwebtoken")
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization")
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({})
-  response.json(blogs.map((blog) => blog.toJSON()))
+  const blogObjects = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+  })
+  const blogs = blogObjects.map((blog) => blog.toJSON())
+  response.json(blogs)
 })
 
 blogsRouter.post("/", async (request, response) => {
   const body = request.body._doc ? request.body._doc : request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if (!body.title && !body.url) {
     response.send(400).end()
@@ -18,9 +38,13 @@ blogsRouter.post("/", async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
+    userId: user._id,
   })
 
   const savedBlog = await blog.save()
+  user.notes = user.notes.concat(savedBlog._id)
+  await user.save()
+
   response.json(savedBlog)
 })
 
